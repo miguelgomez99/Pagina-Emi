687b4b14c424d0389c00b73f5297f275ea47ecac
miguelgomez99 <migue8935@gmail.com>
2025-02-05 00:18:01 -0300

    Casi terminado

--- Script.js
+++ Script.js
@@ -1,28 +1,16 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const dropdownBtn = document.querySelector(".dropdown-btn");
+  const dropdownContent = document.querySelector(".dropdown-content");
 
-let nombreCompleto = (nombre, apellido) => nombre + " " + apellido;
-
-let exclamar = str => str + "!";
-
-let mayusculas = str => str.toUpperCase();
-
-let minusculas = str => str.toLowerCase();
-
-console.log( nombreCompleto('Ada', 'Lovelace') ); // Ada Lovelace
-console.log( exclamar('Hedy') ); // ¡Hedy!
-console.log( mayusculas('grace') ); // GRACE
-console.log( minusculas('SHERYL') ); // sheryl
-
-const longitudDelNombre = (nombre = "") => nombre.length;
-
-console.log( longitudDelNombre() ); // 0
-console.log( longitudDelNombre('Ana') ); // 3
-
-function ejecutar(){
-swal({
-    title: "Good job!",
-    text: "You clicked the button!",
-    icon: "success",
-    button: "Aww yiss!",
+  dropdownBtn.addEventListener("click", function () {
+      dropdownContent.classList.toggle("active");
   });
-}
+
+  // Cerrar menú si se hace clic fuera
+  document.addEventListener("click", function (event) {
+      if (!dropdownBtn.contains(event.target) && !dropdownContent.contains(event.target)) {
+          dropdownContent.classList.remove("active");
+      }
+  });
+});
 
